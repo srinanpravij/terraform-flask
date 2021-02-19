6a528871ed7de6a18b6408b11ad334456ebce840
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2021-02-19 15:50:17 -0500

    checking the image

--- web.py
+++ web.py
@@ -6,4 +6,4 @@ def main():
     return render_template('index.html')
  
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="localhost", port=8080, debug=True)
