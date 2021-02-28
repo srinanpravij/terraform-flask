9e279339f5c8701bb66141b5e44d1b879cb19e26
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2021-02-28 08:31:24 -0500

    changed main.tf

--- main.tf
+++ main.tf
@@ -9,4 +9,35 @@ terraform {
       version = "2.11.0"
     }
   }
+  
+# ---------------------------------------------------------------------------------------------------------------------
+# Provision the worker using remote-exec
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "null_resource" "example_provisioner" {
+  triggers = {
+    public_ip = 192.168.1.208
+  }
+
+  connection {
+    type  = "ssh"
+    host  = 192.168.1.208
+    user  = ubuntu
+    port  = 22
+    agent = true
+  }
+
+ // change permissions to executable and pipe its output into a new file
+  provisioner "remote-exec" {
+    inline = [
+	  "pwd "
+      "mkdir testdirectory",
+      "echo here in remote",
+    ]
+  }
+
+}
+
+  
+  
 }
