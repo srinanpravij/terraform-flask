c63d26f7f4faa193d067bb26ae8dc95654054029
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2021-02-27 20:21:28 -0500

    main.tf changed

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ provider "aws" {
 
  # 1. Create vpc
 
- resource "aws_vpc" "tfprod-vpc" {
-   cidr_block = "10.0.0.0/16"
-   tags = {
-     Name = "tfprod"
-   }
- }
+ #resource "aws_vpc" "tfprod-vpc" {
+ #  cidr_block = "10.0.0.0/16"
+ #  tags = {
+ #    Name = "tfprod"
+ #  }
+ #}
 
 
 
