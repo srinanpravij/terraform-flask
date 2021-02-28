a98ccd3d58bc9480433b553f8d1ebd32e7a72553
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2021-02-28 08:59:56 -0500

    changed main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,33 @@ terraform {
       version = "2.11.0"
     }
   }
-  
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = "https://127.0.0.1:35211"
+  config_path = "/home/ubuntu/.kube/config"
+}
+
+provider "docker" {
+  host = "tcp://localhost:2376"
+}
+
 resource "kubernetes_deployment" "flaskapptf" {
   metadata {
     name = "scalable-flaskapptf-example"
@@ -72,7 +98,4 @@ resource "kubernetes_service" "flaskapptf" {
 
     type = "NodePort"
   }
-
-  
-  
 }
