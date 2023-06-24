2cf000e457ff6c001233672db310685b0cc5dadf
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-06-23 19:20:30 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
